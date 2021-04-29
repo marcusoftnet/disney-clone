16281ece325625f2621697b7a6e79c417eb10802
Marcus Hammarberg <marcusoft.net@gmail.com>
2021-04-29 15:58:30 +0200

    Styled the first page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className='App'>
-      <h2>Hello lets build a Disney clone</h2>
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
